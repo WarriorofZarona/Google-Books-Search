7f145e94a01da8891123b4f7287c272d167fb25d
Andrew Bautista <andrewbautista85@gmail.com>
2020-05-17 17:35:05 -0400

    added ternary operations for active link state depending on pathname

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,22 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 
 function Navbar() {
 
+    const location = useLocation();
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom border-dark">
-            <a className="navbar-brand" href="#">Google Books</a>
+            <Link className="navbar-brand" to="/">Google Books</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Search <span class="sr-only">(current)</span></a>
+                    <li className={location.pathname === "/" ? "nav-item active" : "nav-item"}>
+                        <Link className="nav-link" to="/">Search <span class=" sr-only">(current)</span></Link>
                     </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Saved</a>
+                    <li className={location.pathname === "/saved" ? "nav-item active" : "nav-item"}>
+                        <Link className="nav-link" to="/saved">Saved</Link>
                     </li>
 
                 </ul>
