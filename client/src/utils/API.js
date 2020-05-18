648d22beff45e2c6e2949b9e3af9dc3b236f734f
Andrew Bautista <andrewbautista85@gmail.com>
2020-05-18 17:32:50 -0400

    added front end axios code for get, post, and delete

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,5 +3,14 @@ import axios from "axios";
 export default {
     getBooks: query => {
         return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query)
+    },
+    saveBook: object => {
+        return axios.post("/api/books", object)
+    },
+    getSavedBooks: () => {
+        return axios.get("/api/books");
+    },
+    deleteBook: id => {
+        return axios.delete("/api/books", id);
     }
 };
