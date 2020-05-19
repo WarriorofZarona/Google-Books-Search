f3c5c43022ccc088e277cf482b0e2dd0e2933bcd
Andrew Bautista <andrewbautista85@gmail.com>
2020-05-19 14:06:25 -0400

    simplified the schema to make sure it works properly

--- models/Books.js
+++ models/Books.js
@@ -2,19 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const BookSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    authors: {
-        type: Array,
-        required: true
-    },
-    description: {
-        type: String,
-        trim: true,
-        required: true
-    },
+    title: String,
+    authors: Array,
+    description: String,
     image: String,
     link: String
 });
